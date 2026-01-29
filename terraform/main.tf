adcda0418db8eecc05de07ab8f785f8429597944
Jorge Luis Olivos <jlolivos@gmail.com>
2026-01-29 11:16:16 -0600

    Initial commit for lab01 (cleanup terraform files)

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+	features {}
+	subscription_id = "ceab1529-eb09-47cb-a0bd-d71d761771a0"
+}
+
+resource "azurerm_resource_group" "rg" {
+	name = "rg-aks-devops"
+	location = "eastus"
+}
+
+resource "azurerm_container_registry" "acr" {
+	name 	     	    = "acrdevopsjorge"
+	resource_group_name = azurerm_resource_group.rg.name
+	location	    = azurerm_resource_group.rg.location
+	sku		    = "Basic"
+	admin_enabled	    = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+	name 		    = "aks-devops"
+	location 	    = azurerm_resource_group.rg.location
+	resource_group_name = azurerm_resource_group.rg.name
+	dns_prefix	    = "aksdevops"
+
+	default_node_pool {
+		name	    = "default"
+		node_count  = 2
+		vm_size     = "Standard_DC2s_v3"
+	}
+
+	identity {
+		type = "SystemAssigned"
+	}
+}
+
+resource "azurerm_role_assignment" "aks_acr" {
+	principal_id 		= azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+	role_definition_name 	= "AcrPull"
+	scope 			= azurerm_container_registry.acr.id
+}
